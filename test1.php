77437906069e06aba9617e3ebc768c0e444b211f
Tyler1powe <153703801+Tyler1powe@users.noreply.github.com>
2023-12-15 20:06:33 -0600

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,30 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <title>  </title>
-  <link rel="stylesheet" href="oscars.css"> 
-  <!-- ----------------------------------
-            BIS1523/BIS2523 Documentation
-   Name:
-   Netid:
-   Date:
-   
-   Variables used:
-      <variable name>  <description of data variable will hold>
-  
-   -------------------------------------  -->
-</head>
-<body>
-<h1>Awards Search</h1>
-<section id="leftcol">
-    <form method="post" action="oscars.php">
-        Starting Year: <input type="text" name="startyear" size="4"> <br />
-        Ending Year: <input type="text" name="endyear" size="4"> <br />
-        <input type="submit" name="submit" value="submit">
-    </form>
-</section>
 
-<section id="rightcol">
 <?
 $years = file("year.txt", FILE_IGNORE_NEW_LINES);
 $actors = file("actors.txt", FILE_IGNORE_NEW_LINES);
@@ -50,8 +24,4 @@ print"<table>
            }
 ?>    
     
-</section>
 
-
-</body>
-</html>
